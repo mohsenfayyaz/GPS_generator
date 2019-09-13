70a58a5514f3d99c983047a93e205186860d102e
mohsenfayyaz <mohsen.fayyaz77@gmail.com>
2019-09-13 16:41:59 +0430

    Gaussian Noise added to points

--- src/GPSGenerator/GPSGenerator.java
+++ src/GPSGenerator/GPSGenerator.java
@@ -12,6 +12,7 @@ import com.grum.geocalc.Point;
 import java.awt.*;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Random;
 
 public class GPSGenerator {
     private GraphHopperHandler myGHHandler;
@@ -19,7 +20,7 @@ public class GPSGenerator {
         myGHHandler = new GraphHopperHandler(osmFile);
     }
 
-    public void generate(double latFrom, double lonFrom, double latTo, double lonTo, double sampleRateMeters, double noiseVariance){
+    public void generate(double latFrom, double lonFrom, double latTo, double lonTo, double sampleRateMeters, double noiseStandardDeviation){
         List<PathWrapper> myPathList = myGHHandler.findRoutes(latFrom, lonFrom, latTo, lonTo);
         for (PathWrapper path : myPathList) {
             System.out.println("-----------");
@@ -28,14 +29,38 @@ public class GPSGenerator {
             new MapViewerHandler().drawRouteOnMap(path, Color.BLUE, "GRAPH HOPPER ROUTE");
 
             path = samplePath(path, sampleRateMeters);
-            System.out.println(path.getPoints());
-
-
             new MapViewerHandler().drawRouteOnMap(path, Color.GREEN, "SAMPLED ROUTE");
+
+            path = addNoise(path, noiseStandardDeviation, 0);
+            new MapViewerHandler().drawRouteOnMap(path, Color.GREEN, "NOISY SAMPLED ROUTE");
+
+
+
         }
 
 
     }
+
+    private PathWrapper addNoise(PathWrapper path, double noiseStandardDeviation, double mean) {
+        PointList noisePoints = new PointList();
+        PointList points = path.getPoints();
+        Iterator<GHPoint3D> pointsIterator = points.iterator();
+
+        Random rand = new Random();
+
+        while(pointsIterator.hasNext()) {
+            GHPoint3D currentPoint = pointsIterator.next();
+            double randDistance = rand.nextGaussian() * noiseStandardDeviation + mean;
+            double randBearing = rand.nextDouble() * 360;
+            GHPoint3D randPoint = calcNextPointByBearing(currentPoint.lat, currentPoint.lon, randBearing, randDistance);
+            noisePoints.add(randPoint);
+        }
+
+        PathWrapper noisePath = new PathWrapper();
+        noisePath.setPoints(noisePoints);
+        return noisePath;
+    }
+
 
     private PathWrapper samplePath(PathWrapper path, double sampleRateMeters){
         PointList samplingPoints = new PointList();
@@ -99,13 +124,22 @@ public class GPSGenerator {
     }
 
     private GHPoint3D calcNextPointBetweenPoints(double latFrom, double lonFrom, double latTo, double lonTo, double distance){
-        //Kew
         Coordinate lat = Coordinate.fromDegrees(latFrom);
         Coordinate lng = Coordinate.fromDegrees(lonFrom);
         Point startPoint = Point.at(lat, lng);
 
         //Distance away point, bearing is from start to destination
         Point otherPoint = EarthCalc.pointAt(startPoint, calcBearing(latFrom, lonFrom, latTo, lonTo), distance);
+        return new GHPoint3D(otherPoint.latitude, otherPoint.longitude, 0);
+    }
+
+    private GHPoint3D calcNextPointByBearing(double latFrom, double lonFrom, double bearing, double distance){
+        Coordinate lat = Coordinate.fromDegrees(latFrom);
+        Coordinate lng = Coordinate.fromDegrees(lonFrom);
+        Point startPoint = Point.at(lat, lng);
+
+        //Distance away point, bearing is from start to destination
+        Point otherPoint = EarthCalc.pointAt(startPoint, bearing, distance);
         return new GHPoint3D(otherPoint.latitude, otherPoint.longitude, 0);
     }
 }
